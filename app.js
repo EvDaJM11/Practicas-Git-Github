bec34ef1fda524e16e481c624b4ff56cae01bdc5
Daniel Jimenez <160152237+EvDaJM11@users.noreply.github.com>
2024-07-07 14:08:58 -0600

    (proyecto)(Inicio proyecto)
    Instalacion de proyecto inicial

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Primeros cambios en repositorio");
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el n�mero');
+}
