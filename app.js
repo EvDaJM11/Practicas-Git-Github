ac22a23aed122eee0e7cde5d0a13348b2c7200b7
Daniel Jimenez <160152237+EvDaJM11@users.noreply.github.com>
2024-07-22 11:33:53 -0600

    (cambios generales)(proyecto)

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un n�mero por favor:");
-
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el n�mero');
-}
+alert("Hola mundo");
