936516bf75cab8288b5fe006704214187a5644da
Daniel Jimenez <160152237+EvDaJM11@users.noreply.github.com>
2024-07-22 12:23:21 -0600

    (index.html - app.js)(cambios generales)

---
+++ curso-logica/app.js
@@ -0,0 +1,12 @@
+var titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego de numero secreto"
+
+let parrafo = document.querySelector('p')
+parrafo.innerHTML = "Indica un numero del uno al 10"
+
+function intentar(){
+
+}
+function reintentar(){
+
+}
